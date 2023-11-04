ee576bb331bf351624c31e9d6d68a3c7da82e4c9
Akshansh <akshanshkul7830@gmail.com>
2023-11-04 15:51:06 +0530

    add

--- officeOfDean.php
+++ officeOfDean.php
@@ -6,55 +6,7 @@ include 'header.php';
         <?php
         include 'left-block.php';
         ?>
-        <style>
-            .profile-card {
-                min-width: 200px;
-                max-width: 400px;
-                min-height: 200px;
-                max-height: 400px;
-                background-color: green;
-                border-radius: 5px;
-                padding: 10px;
-                display: flex;
-                justify-content: space-around;
-                align-self: center;
-                align-content: center;
-                align-items: center;
-                flex-direction: row;
-                flex-wrap: nowrap;
-            }
-
-            .profile-pic {
-                width: 130px;
-                height: 130px;
-                border-radius: 50%;
-                background-color: white;
-            }
-
-            .profile-pic img {
-                width: 130px;
-                height: 130px;
-                border-radius: 50%;
-
-            }
-
-            .information .name {
-                color: white;
-                font-size: 18px;
-                font-weight: bold;
-            }
-
-            .information
-            .position,
-            .mobile,
-            .email {
-                color: white;
-                font-size: 14px;
-            }
-            .information .email{
-                font-style: italic;
-            }
-        </style>
+      
         <div class="right">
             <div class="card">
                 <div class="heading">Office of the Dean Student Welfare</div>
